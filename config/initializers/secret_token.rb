d1c997ea3832135cf4e27035e6998f283ff191c0
bryanl <bryan@osesm.com>
2014-01-17 20:22:37 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gruntapp::Application.config.secret_key_base = 'f4b168eb03bb3c7adc1657d26172c0a0f3ce8f4ab5df2abf51376754a60ba99a3d5bd41c6756c4c17b9d0d88fa6252c53e12f4ba88500135f2642322b709c244'
